e69a6b24a3817befc1cde024ca2268b2e3c962ca
hirakikatsuya <h.k.56781075@gmail.com>
2023-01-26 01:28:17 +0900

    スッキリJava2入門2章終了

---
+++ sukkiri-java-practice/src/practice/Practice1_3.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Practice1_3 {
+	public static void main(String[] args) {
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた！";
+		System.out.println(favoriteCharacter + pi + number + message + result);
+	}
+}
